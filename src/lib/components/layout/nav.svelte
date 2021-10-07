bf3d281b242b0e70cd85399ce8e307731422eea6
Your Name <avneeshagarwal0612@gmail.com>
2021-10-07 21:33:47 +0530

    added discord link

--- src/lib/components/layout/nav.svelte
+++ src/lib/components/layout/nav.svelte
@@ -2,6 +2,12 @@
 	<ul>
 		<li><a href="/">Home</a></li>
 	</ul>
+
+	<ul>
+		<a href="https://discord.com/invite/vM2bagU" target="_blank" rel="noopener noreferrer"
+			><img class="discord" src="/static/img/discord.svg" alt="discord" /></a
+		>
+	</ul>
 </nav>
 
 <style>
@@ -10,6 +16,16 @@
 		align-items: center;
 		gap: 0.5rem;
 		color: var(--white);
+	}
+
+	.discord {
+		height: 2rem;
+		width: 2rem;
+	}
+
+	.discord:hover {
+		transform: scale(1.1);
+		transition: transform 0.2s ease-in-out;
 	}
 
 	ul {
