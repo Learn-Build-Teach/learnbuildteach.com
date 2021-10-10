0157d9af3d5677d25f0b28ac547bd61a811c5096
brittneypostma <brittney.postma@hotmail.com>
2021-10-10 09:46:46 -0400

    added link style and hover on home

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,20 @@
-<h1>Welcome to Learn Build Teach</h1>
-<p>
-	Visit <a href="https://github.com/Learn-Build-Teach/learnbuildteach.com">Learn Build Teach repo</a
-	> to help contribute!
-</p>
+<article>
+	<h1>Welcome to Learn Build Teach</h1>
+	<p>
+		Visit <a href="https://github.com/Learn-Build-Teach/learnbuildteach.com"
+			>Learn Build Teach repo</a
+		> to help contribute!
+	</p>
+</article>
+
+<style>
+	a {
+		border-bottom: 1px solid var(--secondary);
+		transition: var(--transition-links);
+	}
+
+	a:hover {
+		filter: brightness(1.05);
+		border-bottom: 2px solid var(--secondary);
+	}
+</style>
