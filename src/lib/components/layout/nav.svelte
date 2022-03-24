c9219bc5c59c47b8e325eb9dc765bceb4f99313c
Brian Morrison II <brian@brianmorrison.me>
2022-03-24 15:35:14 -0500

    Comment out login, update login url

--- src/lib/components/layout/nav.svelte
+++ src/lib/components/layout/nav.svelte
@@ -2,7 +2,7 @@
 	import { page } from '$app/stores';
     $: path = $page.url.pathname;
 
-	let loginUrl = "https://discord.com/api/oauth2/authorize?client_id=787509396447166474&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Foauth&response_type=code&scope=identify%20email";
+	let loginUrl = `https://discord.com/api/oauth2/authorize?client_id=787509396447166474&redirect_uri=${$page.url.origin}%2Foauth&response_type=code&scope=identify%20email`;
 </script>
 
 <nav>
