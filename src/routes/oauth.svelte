d7c1192d82d36e8621ece1f86ed2c345956d89fd
James Q Quick <james.q.quick@gmail.com>
2022-03-25 09:14:55 -0500

    Test log

--- src/routes/oauth.svelte
+++ src/routes/oauth.svelte
@@ -1,27 +1,26 @@
 <script>
-  import { page } from '$app/stores';
-  import { goto } from '$app/navigation';
-  const code = $page.url.searchParams.get('code');
+	import { page } from '$app/stores';
+	import { goto } from '$app/navigation';
+	const code = $page.url.searchParams.get('code');
 
-  async function processAuthCode() {
-    let res = await fetch("/api/tokens", {
-      method: "post",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ code })
-    });
-    if(res.ok) {
-      let json = await res.json()
-      localStorage.setItem("token", json.access_token)
-    } else {
-      // TODO: Handle this
-    }
-    goto("/")
-  }
-  processAuthCode()
+	async function processAuthCode() {
+		let res = await fetch('/api/tokens', {
+			method: 'post',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ code })
+		});
+		if (res.ok) {
+			let json = await res.json();
+			localStorage.setItem('token', json.access_token);
+		} else {
+			// TODO: Handle this
+			console.log("We didn't get a token for some odd reason :)");
+		}
+		goto('/');
+	}
+	processAuthCode();
 </script>
 
-<article>
-  Loading...
-</article>
+<article>Loading...</article>
