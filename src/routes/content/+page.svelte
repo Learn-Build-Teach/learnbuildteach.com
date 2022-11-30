9963160802531704cc725545e59c7375b058e1ef
jamesqquick <james.q.quick@gmail.com>
2022-11-30 15:10:40 -0600

    Updated reference to content value from store

--- src/routes/content/+page.svelte
+++ src/routes/content/+page.svelte
@@ -5,7 +5,7 @@
 <article>
 	<h1>Recent Content from the LBT Community!</h1>
 	<ul>
-		{#each content as item}
+		{#each $content as item}
 			<li>
 				<a target="_blank" rel="noreferrer" href={item.link}>
 					{item.title} by {item.user.username}
