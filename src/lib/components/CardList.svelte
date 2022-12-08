a885e965958bba8a5387e2fc602c3ccf407051db
The Noah <thenoahbz@gmail.com>
2022-12-08 14:15:28 -0600

    fix: make content and admin pages consistent

---
+++ src/lib/components/CardList.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  export let cards: any[];
+</script>
+
+<ul>
+  {#each cards as card}
+    <slot {card} />
+  {/each}
+</ul>
+
+<style>
+	ul {
+		padding-left: 0px;
+		display: flex;
+		flex-wrap: wrap;
+		gap: var(--gap-6);
+	}
+</style>
