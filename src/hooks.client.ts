081941e5a5adf7c11e4769173cb4ad0e78917f8c
jamesqquick <james.q.quick@gmail.com>
2023-03-28 14:59:19 -0500

    referncing public app domain env variable for highlight config

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,10 +1,10 @@
 import { H } from 'highlight.run';
-import { PUBLIC_HIGHLIGHT_PROJECT_ID } from '$env/static/public';
+import { PUBLIC_HIGHLIGHT_PROJECT_ID, PUBLIC_APP_DOMAIN } from '$env/static/public';
 
 H.init(PUBLIC_HIGHLIGHT_PROJECT_ID, {
 	environment: 'production',
 	version: 'commit:abcdefg12345',
-	tracingOrigins: ['localhost'],
+	tracingOrigins: [PUBLIC_APP_DOMAIN],
 	networkRecording: {
 		enabled: true,
 		recordHeadersAndBody: true,
