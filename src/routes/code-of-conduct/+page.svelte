3036bd6cfa8260932ddbdb890a61b0807ea30358
wipdev-tech <wipdev.tech@gmail.com>
2023-11-09 18:25:06 +0200

    !chore: ported code of conduct to tailwind

--- src/routes/code-of-conduct/+page.svelte
+++ src/routes/code-of-conduct/+page.svelte
@@ -1,23 +1,23 @@
 <article>
-	<h1>Code of Conduct</h1>
-	<p>
+	<h1 class="text-4xl font-black mb-4">Code of Conduct</h1>
+	<p class="mb-4">
 		Welcome! The goal of this Learn Build Teach server is to provide a safe place for developers to
 		well... learn, build, and teach! Feel free to share projects that you're working on, questions
 		that you have, content you've created, etc. I think that we all have a lot we can learn from
 		each other, and that's what we would like to build a community around.
 	</p>
 
-	<h2>Admins</h2>
+	<h2 class="text-2xl mb-2">Admins</h2>
 
-	<p>
+	<p class="mb-4">
 		We want this to be a fun, pleasant, and harassment-free experience for everyone, regardless of
 		gender, gender identity and expression, sexual orientation, disability, physical appearance,
 		body size, race, or religion. We do not tolerate harassment of participants in any form.
 		Participants asked to stop any harassing behavior are expected to comply immediately.
 	</p>
 
-	<p>For admin support, please reach out to one of the following admins</p>
-	<ul>
+	<p class="mb-4">For admin support, please reach out to one of the following admins</p>
+	<ul class="list-disc pl-10 my-6 space-y-2">
 		<li>James Quick</li>
 		<li>Brittney Postma</li>
 		<li>Ryan McGovern</li>
@@ -25,11 +25,10 @@
 		<li>Amy Dutton</li>
 	</ul>
 
-	<h2>Harassment</h2>
-	<p>Harassment includes:</p>
-	<br />
+	<h2 class="text-2xl mb-2">Harassment</h2>
+	<p class="mb-4">Harassment includes:</p>
 
-	<ul>
+	<ul class="list-disc pl-10 my-6 space-y-2">
 		<li>
 			Offensive comments related to gender, gender identity and expression, sexual orientation,
 			disability, mental illness, neuro(a)typicality, physical appearance, body size, race, or
@@ -59,57 +58,57 @@
 		<li>Publication of non-harassing private communication</li>
 	</ul>
 
-	<h2>Following Tutorials and Building Projects</h2>
+	<h2 class="text-2xl mb-2">Following Tutorials and Building Projects</h2>
 
-	<p>
+	<p class="mb-4">
 		Following tutorials and other learning materials is core to building your skills as a developer.
 		However, as you follow said tutorials, I think it’s important to remember 2 things to ensure
 		that we are learning in the most effective way and giving credit to the original content
 		creators.
 	</p>
 
-	<p>
+	<p class="mb-4">
 		Always be sure to credit whoever the creator of the content is if you decide to publish a
 		similar project on your Github, portfolio, etc.
 	</p>
 
-	<p>
+	<p class="mb-4">
 		To ensure you get the most out of the material, I always recommend that you take the things you
 		learn in a tutorial and apply them by building your own unique project. By building your own
 		project, you internalize the things you learned and create a unique example to include on your
 		portfolio.
 	</p>
 
-	<h2>Reporting</h2>
+	<h2 class="text-2xl mb-2">Reporting</h2>
 
-	<p>
+	<p class="mb-4">
 		If you are being harassed, notice that someone else is being harassed, or have any other
 		concerns, please contact the admins. They'll respond as promptly as they can.
 	</p>
 
-	<p>
+	<p class="mb-4">
 		We will respect confidentiality requests for the purpose of protecting victims of abuse. At our
 		discretion, we may publicly name a person about whom we’ve received harassment complaints, or
 		privately warn third parties about them. We will not name harassment victims without their
 		affirmative consent.
 	</p>
 
-	<h3>Consequences</h3>
-	<p>Participants asked to stop any harassing behavior are expected to comply immediately.</p>
+	<h3 class="text-xl">Consequences</h3>
+	<p class="mb-4">Participants asked to stop any harassing behavior are expected to comply immediately.</p>
 
-	<p>
+	<p class="mb-4">
 		If a participant engages in harassing behavior, the admins may take any action they deem
 		appropriate, up to and including expulsion from this Discord and identifying the participant
 		publicly as someone about whom we've received complaints.
 	</p>
 
-	<h2>Credits and License</h2>
-	<p>
+	<h2 class="text-2xl mb-2">Credits and License</h2>
+	<p class="mb-4">
 		[This work is available to you to use, publish, and modify under the terms of the Creative
 		Commons Attribution License. Please keep the paragraph below attached to your final version.]
 	</p>
 
-	<p>
+	<p class="mb-4">
 		This code of conduct is based on Annalee Flower Horne's Sample Slack Code of Conduct, which is
 		in turn based on Geek Feminism's Community Anti-Harassment Policy. We have adapted the Sample
 		Slack Code of Conduct for our own use under the terms of the Creative Commons Attribution
