3b61a4c78782aab85a2dcfa641dfc3a978eca4c9
wipdev-tech <wipdev.tech@gmail.com>
2023-11-09 19:04:20 +0200

    feat: added dummy content cards for local dev
    This is first-pass way of making the share cards visible for local
    development that doesn't rely on the database.

--- src/routes/content/+page.svelte
+++ src/routes/content/+page.svelte
@@ -3,16 +3,22 @@
 	import CardList from '$components/CardList.svelte';
 	import { onMount } from 'svelte';
 	import { loadShares, shares } from '$stores/sharesStore';
+	import { supabase } from '$src/lib/helpers/supabase';
+	import DummyCard from './DummyCard.svelte';
 
 	onMount(async () => {
-		loadShares();
+		if (supabase) {
+			loadShares();
+		}
 	});
 
 	let page = 0;
 
 	const handleLoadMore = () => {
-		page++;
-		loadShares(page);
+		if (supabase) {
+			page++;
+			loadShares(page);
+		}
 	};
 </script>
 
@@ -23,6 +29,22 @@
 		<CardList cards={$shares} let:card>
 			<ShareCard share={card} />
 		</CardList>
+	{:else}
+		<ul>
+			<DummyCard />
+			<DummyCard />
+			<DummyCard />
+			<DummyCard />
+		</ul>
 	{/if}
 	<button on:click={handleLoadMore}>Load More</button>
 </article>
+
+<style>
+	ul {
+		padding-left: 0px;
+		display: flex;
+		flex-wrap: wrap;
+		gap: var(--gap-6);
+	}
+</style>
---
+++ src/routes/content/DummyCard.svelte
@@ -0,0 +1,38 @@
+<script>
+	const dateString = new Date().toLocaleDateString('en-us', {
+		weekday: 'long',
+		year: 'numeric',
+		month: 'short',
+		day: 'numeric'
+	});
+</script>
+
+<div class="share-card">
+	<img class="card-image" alt="Share description" />
+	<div class="card-body">
+		<p>{dateString}</p>
+		<a target="_blank" rel="noreferrer" href="about:blank">
+			<h2>Lorem ipsum dolor sit amet</h2>
+		</a>
+		<p>by Lorem Ipsum</p>
+		<slot />
+	</div>
+</div>
+
+<style>
+	.share-card {
+		background: var(--primary-dark);
+		border-radius: var(--gap-2);
+		overflow: hidden;
+		width: 300px;
+	}
+	.card-image {
+		height: 200px;
+		width: 100%;
+		object-fit: cover;
+        background: #457b9d;
+	}
+	.card-body {
+		padding: var(--gap-6);
+	}
+</style>
