4aa9ff60cd80c6286f11b1089a810f29abd6b546
wipdev-tech <wipdev.tech@gmail.com>
2023-11-21 20:17:43 +0200

    !chore(admin): checkbox style ported to tailwind

--- src/lib/components/checkbox.svelte
+++ src/lib/components/checkbox.svelte
@@ -2,62 +2,38 @@
   export let checked = false;
 </script>
 
-<label>
-  <div class="checkbox" class:checked>
+<label class="flex items-center cursor-pointer">
+  <div class="checkbox w-6 h-6 bg-white border border-gray-600 rounded mr-3" class:checked>
     {#if checked}
-      <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"
+            class="w-full h-full">
         <path d="M5 10.273 10.833 16 20 7" stroke="#fff" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
       </svg>
     {/if}
   </div>
-  <input type="checkbox" bind:checked on:change />
+  <input class="hidden" type="checkbox" bind:checked on:change />
 
-  <div class="text">
+  <div class="select-none">
     <slot />
   </div>
 </label>
 
-<style>
-  input[type=checkbox] {
-    display: none;
-  }
-
-  label {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    margin: 0;
-    cursor: pointer;
-  }
-
+<style lang="postcss">
   .checkbox {
-    width: 1.5rem;
-    height: 1.5rem;
-    margin-right: var(--gap-3);
-    background-color: var(--white);
-    border: 1px solid var(--gray);
-    border-radius: var(--gap-1);
     transition-property: background-color, border-color;
     transition-duration: 150ms;
-    transition-timing-function: var(--cb);
+    transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
   }
 
   .checkbox.checked {
-    background-color: var(--secondary);
-    border-color: var(--secondary);
+    @apply bg-secondary border-secondary;
   }
 
   .checkbox svg {
-    width: 100%;
-    height: 100%;
     stroke-dasharray: 26;
     stroke-dashoffset: 26;
     animation: check-draw 150ms linear forwards;
     animation-delay: 75ms;
-  }
-
-  .text {
-    user-select: none;
   }
 
   @keyframes check-draw {
