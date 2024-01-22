561c83ccbb830b5cdcba9ee3e70aade06ecbacf1
Riley1101 <arkardev00@gmail.com>
2024-01-22 14:08:50 -0500

    update on event detail page design

--- src/lib/components/storyblok/PinnedEvent.svelte
+++ src/lib/components/storyblok/PinnedEvent.svelte
@@ -17,18 +17,18 @@
 	<div
 		class="w-full -z-[8] h-full absolute top-0 left-0 rounded-xl bg-gradient-to-t from-black/20"
 	/>
-	<div class="px-8 z-0" use:storyblokEditable={blok}>
+	<div class="px-4 md:px-8 z-0" use:storyblokEditable={blok}>
 		<span class="text-white bg-secondary px-2 font-bold mb-2 inline-block py-1 text-xs rounded-full"
 			>{blok.title}</span
 		>
 		<h1 class="font-heading font-black text-4xl my-2 text-white">{event.content.name}</h1>
 		<p class="mb-8 text-gray-300">{event.content.description}</p>
-		<div class="flex gap-4">
+		<div class="flex flex-col md:flex-row gap-4">
 			<a
 				href={blok.cta_url.url}
 				target="_blank"
 				rel="noreferrer noopener"
-				class="bg-white border-none block max-w-max py-2 px-4 mb-4 rounded-lg text-primary hover:bg-accent transition-500"
+				class="bg-white border-none block max-w-max py-2 px-4 md:mb-4 rounded-lg text-primary hover:bg-accent transition-500"
 			>
 				{blok.cta_title}
 			</a>
--- src/routes/events/[slug]/+page.svelte
+++ src/routes/events/[slug]/+page.svelte
@@ -11,18 +11,14 @@
 <div use:storyblokEditable={story}>
 	<div class="container mx-auto mb-12">
 		<div
-			class="flex flex-col justify-end py-12 border p-4 rounded-xl bg-primary-dark/50 relative min-h-[400px] border-primary-dark/20 backdrop-blur"
+			class="flex flex-col justify-end py-12 border p-4 rounded-xl bg-primary-dark/20 relative  border-primary-dark/10 backdrop-blur"
 		>
-			<Image
-				image={content.cover}
-				class="-z-10 w-full rounded-xl top-0 h-full left-0 absolute object-cover gradient-mask"
-			/>
 			<div
-				class="w-full -z-[8] h-full absolute top-0 left-0 rounded-xl bg-gradient-to-b from-black/70 backdrop-blur-sm"
+				class=" w-full -z-[8] h-full absolute top-0 left-0 rounded-xl bg-gradient-to-b from-black/50 backdrop-blur-sm"
 			/>
 			<div class="px-8 z-0">
 				<h1 class="font-heading font-black text-4xl my-2 text-white">{content.name}</h1>
-				<p class="mb-8 text-gray-300">{content.description}</p>
+				<p class="text-gray-300">{content.description}</p>
 			</div>
 		</div>
 
@@ -37,20 +33,11 @@
 					@ {formatTime(content.start_date)}
 				</span>
 			</span>
-
-			<span
-				class="px-4 p-2 text-sm  p bg-primary-dark/30 rounded-xl backdrop-blur-lg hover:bg-primary-dark/90 cursor-pointer"
-			>
-				<span>
-					{formatDate(content.start_date)}
-				</span>
-				<span class="font-semibold text-secondary">
-					@ {formatTime(content.start_date)}
-				</span>
-			</span>
 		</div>
 		<a
-			href="/events/{content.slug}"
+			href={content.url.url}
+			target="_blank"
+			rel="noopener noreferrer"
 			class="mb-8 hover:bg-secondary/100 text-lg bg-secondary/90 text-white border-none block max-w-max py-2 px-4  rounded-lg  transition-500 backdrop-filter backdrop-blur-lg"
 		>
 			Join now
@@ -62,16 +49,21 @@
 					{#if content.quick_note}
 						<p class="mb-8 text-gray-300">{content.quick_note}</p>
 					{/if}
-					<span class="font-bold text-white"> Share now </span>
-					<ul class="mt-2">
-						{#each content.socials as social}
-							<li>
-								<a href={social?.content?.url?.url} aria-label={social.name}>
-									<Image image={social?.content?.icon} class="w-6 h-6 " />
-								</a>
-							</li>
-						{/each}
-					</ul>
+					{#if content.socials}
+						<span class="font-bold text-white"> Follow us</span>
+						<ul class="mt-2 flex items-center gap-2">
+							{#each content.socials as social}
+								<li>
+									<a href={social?.content?.url?.url} class="group" aria-label={social.name}>
+										<Image
+											image={social?.content?.icon}
+											class="w-6 h-6 group-hover:scale-110 transition-transform object-cover"
+										/>
+									</a>
+								</li>
+							{/each}
+						</ul>
+					{/if}
 				</div>
 			</div>
 		</div>
